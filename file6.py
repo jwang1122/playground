ac67a462d958cb820cf0d3ab299e234bdab42fcf
John Wang <wangqianjiang@live.com>
2020-04-12 13:32:16 -0500

    books.json

--- file6.py
+++ file6.py
@@ -10,7 +10,7 @@ date2 = []
 texasConfirmed = []
 
 first = True
-with open('data_minimal.csv', 'r') as covid_19:
+with open('data_minimal.csv', 'r') as covid_19: #CSV: Comma Separated Values 
     reader = csv.reader(covid_19)
     last = ''
     for row in covid_19:
@@ -26,19 +26,19 @@ with open('data_minimal.csv', 'r') as covid_19:
             texasConfirmed.append(confirmed)
     covid_19.close()
 
-# print(len(date1))
-# print(len(newyorkConfirmed))
+print(date1)
+print(newyorkConfirmed)
 # print(len(date2))
 fig=plt.figure()
 ax=fig.add_subplot(111)
 
 x = date1
 y = newyorkConfirmed
-ax.plot(x,y,c='r',marker=(8,2,0),ls='-',label='New York')
+ax.plot(x,y,c='r',marker='*',ls='-',label='New York')
 ax.plot(date2,texasConfirmed,c='g',marker='v',ls='-',label='Texas')
 labels = ax.get_xticklabels()
 plt.setp(labels, rotation=90, horizontalalignment='right')
-ax.set(xlim=['2020-03-15','2020-04-04'], xlabel='Date', ylabel='Confirmed',
+ax.set(xlim=['2020-03-04','2020-04-04'], xlabel='Date', ylabel='Confirmed',
        title='Coronavirus Report')
-plt.legend(loc=2)
+plt.legend(loc=1)
 plt.show()
