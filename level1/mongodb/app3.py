6761ef7b8082fa5190cacd78dcfc649a02e90ed9
John Wang <wangqianjiang@live.com>
2020-06-24 07:04:49 -0500

    Modify sample code

--- level1/mongodb/app3.py
+++ level1/mongodb/app3.py
@@ -1,29 +1,31 @@
-#import uuid
+import uuid
 from flask import Flask, jsonify, request
-
+import json
 
 BOOKS = [
     {
-        '_id': '12345',
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True,
-        'price': '19.99'
+        'price': '19.99',
+        'rating':3
     },
     {
-        '_id':'23456',
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False,
-        'price': '9.99'      
+        'price': '9.99',
+        'rating':5      
     },
     {
-        '_id': '34567',
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True,
-        'price': '4.99'
-
+        'price': '4.99',
+        'rating':1
     }
 ]
 
@@ -34,19 +36,17 @@ BOOKS = [
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
-    return '<html><body><h1>pong!</h1></body></html>'
-
+    return jsonify('pong!')
 
 @app.route('/books', methods=['GET'])
 def all_books():
     response_object = {'status': 'success'}
     response_object['books'] = BOOKS
     return jsonify(response_object)
-
-if __name__ == "__main__":
+    
+if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host="localhost", port=5000)
--- level1/mongodb/bookdb.py
+++ level1/mongodb/bookdb.py
@@ -3,6 +3,70 @@ import json
 import uuid
 
 class BookDB:
+    """
+    Implementation of CRUD for Book class
+    """
+    def __init__(self,dbname,url):
+        self.dbname = dbname
+        self.url = url
+
+    def getBookDB(self):
+        client = MongoClient(self.url)
+        db = client[self.dbname]
+        bookdb = db.books
+        return bookdb
+
+    def getBooks(self):
+        bookdb = self.getBookDB()
+        result = bookdb.find()
+        bookList = []
+        for book in result:
+            bookList.append(book)
+        return bookList
+
+    # Create
+    def create(self, book):
+        """
+        Create a book in database
+        """
+        db = self.getBookDB()
+        result = db.insert_one(book)
+        return result.inserted_id
+
+    # Retrieve
+    def getBook(self, _id):
+        """
+        Retrieve a book from database by _id
+        """
+        db = self.getBookDB()
+        result = db.find_one({'_id':_id})
+        return result
+
+    # Update
+    def update(self, book):
+        """
+        Update one record in database
+        """
+        self.delete(book['_id'])
+        self.create(book)
+
+    # Delete
+    def delete(self, book_id):
+        """
+        Delete a book by _id
+        """
+        db = self.getBookDB()
+        result = db.find_one_and_delete({'_id':book_id})
+        return book_id
+        
+    def addBooks(self, all_books):
+        bookdb = self.getBookDB()
+        results = bookdb.insert_many(all_books)
+        for id in results.inserted_ids:
+            print("Books Added. The course Id is", str(id))
+
+
+if __name__ == '__main__':
     all_books = [
         {
             "_id": uuid.uuid4().hex,
@@ -29,71 +93,14 @@ class BookDB:
             "rating": 1
         }
     ]
-    def __init__(self,dbname,url):
-        self.dbname = dbname
-        self.url = url
-
-    def getBookDB(self):
-        client = MongoClient(self.url)
-        db = client[f'{self.dbname}']
-        bookdb = db.books
-        return bookdb
-
-    def getBooks(self):
-        bookdb = getBookDB()
-        result = bookdb.find()
-        bookList = []
-        for book in result:
-            bookList.append(book)
-        return bookList
-
-    def create(self, book):
-        """
-        Create a book in database
-        """
-        db = self.getBookDB()
-        result = db.insert_one(book)
-        return result.inserted_id
-
-    def getBook(self, _id):
-        """
-        Retrieve a book from database by _id
-        """
-        db = self.getBookDB()
-        result = db.find_one({'_id':_id})
-        return result
-
-    def update(self, book):
-        """
-        Update one record in database
-        """
-        self.delete(book['_id'])
-        self.create(book)
-
-    def delete(self, book_id):
-        """
-        Delete a book by _id
-        """
-        db = self.getBookDB()
-        result = db.find_one_and_delete({'_id':book_id})
-        return book_id
-        
-    def addBooks(self, all_books):
-        bookdb = self.getBookDB()
-        results = bookdb.insert_many(all_books)
-        for id in results.inserted_ids:
-            print("Books Added. The course Id is", str(id))
-
-
-if __name__ == '__main__':
-# insert initial book sets
-#    addBooks(all_books)
-# get all books from mongodb
-    # books = getBooks()
-    # for book in books:
-    #     print(book)
     db = BookDB('mydb','mongodb://localhost:27017')
-    book = db.getBook("5ee95435aebd4e3da5b95b17")
-    book["price"]=100.99
-    db.update(book)
-    print(book)
+    # insert initial book sets
+    # db.addBooks(all_books)
+    # get all books from mongodb
+    books = db.getBooks()
+    for book in books:
+        print(book)
+    # book = db.getBook("5ee95435aebd4e3da5b95b17")
+    # book["price"]=100.99
+    # db.update(book)
+    # print(book)
