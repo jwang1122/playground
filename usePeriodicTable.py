09699b5001d11cbed9269d238130f2e754d2aeaa
John Wang <wangqianjiang@Johns-MacBook-Pro.local>
2020-06-28 06:51:57 -0500

    map dictionary for periodic table.

--- usePeriodicTable.py
+++ usePeriodicTable.py
@@ -3,15 +3,20 @@ from periodic import *
 
 periodicFile = open("./data/PeriodicTableJSON.json")
 data = json.load(periodicFile)
-print(type(data))
+print("06:",type(data))
+
 elements = data.get("elements")
+print("09:", type(elements))
 
-elementDict = dict()
-for e in elements:
-    elementObject = Element.from_dict(e)
-    elementDict[e["name"]]= elementObject
+elementDict = dict(map(lambda e: (e["name"], Element.from_dict(e)), elements))
+# elementDict = dict()
+# for e in elements:
+#     elementObject = Element.from_dict(e)
+#     elementDict[e["name"]]= elementObject
 
+print(elementDict)
 element = elementDict.get("Uranium")
 print(element.name)
 print(element.atomic_mass)
 print(element.period)
+print(element.shells)
